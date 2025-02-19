55201f44b3ec77b4e6db93decc2228f51163f787
samihere2018 <samihere@crimson.ua.edu>
2025-02-18 21:29:20 -0600

    additions and changes

---
+++ cpp_programming/first_program.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //this line and the next should always begin in a c++ file
+using namespace std; //
+
+int main(){
+    cout<<"Hello Sylvia Dogbatsey, this is your first c-plus-plus file. \n";
+    return 0; //always end your program with a return statement
+}
+
+//To run the above in a MACOS or Windows terminal, use the following command: g++ -o firstprogram first_program.cpp & first_program.exe
