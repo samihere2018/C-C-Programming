54595a7a0163b90e19c6a95ea480371b6230bf29
samihere2018 <samihere@crimson.ua.edu>
2025-03-23 00:11:14 -0500

    additions

---
+++ cpp_programming/ifElse.cpp
@@ -0,0 +1,30 @@
+/* Date: Wednesday, 22nd March, 2025 */
+/* Author: Sylvia Dogbatsey */
+/* Description: Second program in C++ */
+
+/* To run the program in a MACOS or Windows terminal, use the following command: */
+/* g++ -o dummyname filename.cpp */
+/* Next run the dummy name: ./dummyname */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int x = 20;
+    if (x<15){
+        cout<< "Good morning!\n";
+    }
+    else{
+        cout<< "Good evening!\n";
+    }
+
+    /*alternatively: shorthand if...else statement*/
+    // variable = (condition) ? expressionTrue : expressionFalse;
+    int time  = 16;
+    string result = (time<18) ? "Good day\n" : "Goodnight\n";
+    cout<< result;
+    return 0;
+}
