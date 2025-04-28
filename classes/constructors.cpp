1abec0f294f90b08529865365365358739c642d0
samihere2018 <samihere@crimson.ua.edu>
2025-04-28 16:29:58 -0500

    new topic - classes

---
+++ classes/constructors.cpp
@@ -0,0 +1,20 @@
+/* Date: 28th April, 2025 */
+/* Author: Sylvia Dogbatsey */
+
+/* To run the program in a MACOS or Windows terminal, use the following command: */
+/* g++ -o dummyname filename.cpp */
+/* Next run the dummy name: ./dummyname */
+
+/* Topic: Classes - Constructors*/
+/*        A constructor in C++ is a special method that is automatically called when an object of a class is created.
+          To create a constructor, use the same name as the class, followed by parentheses (): */
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
