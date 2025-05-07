ba7da05dd16b3a45636dc3a5fcd7fb183745ecc6
samihere2018 <samihere@crimson.ua.edu>
2025-05-07 12:35:13 -0500

    7-May-2025

---
+++ classes/dateTime.cpp
@@ -0,0 +1,20 @@
+/* Date: 7th May, 2025 */
+/* Author: Sylvia Dogbatsey */
+
+/* To run the program in a MACOS or Windows terminal, use the following command: */
+/* g++ -o dummyname filename.cpp */
+/* Next run the dummy name: ./dummyname */
+
+/* Topic: Classes - Date */
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
