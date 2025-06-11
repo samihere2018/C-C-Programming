f6782432237fc89e626aa0ae6c62f92b22b483bf
Sylvia Amihere <samihere@crimson.ua.edu>
2025-06-11 09:57:49 -0500

    edits

---
+++ examples/example9.cpp
@@ -0,0 +1,26 @@
+/* Date: 9th June, 2025 */
+/* Author: Sylvia Dogbatsey */
+
+/* To run the program in a MACOS or Windows terminal, use the following command: */
+/* g++ -o dummyname filename.cpp */
+/* Next run the dummy name: ./dummyname */
+
+/* Topic: Example 9 - Use the weekday number to calculate and output the weekday name */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    int numberDay;
+    cout<<"Enter any number from 1 to 7 corresponding to the da
+
+
+
+    return 0;
+}
+
