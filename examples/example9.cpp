8187ce6f0231e81d73b73c2f9106680b22d872f8
Sylvia Amihere <samihere@crimson.ua.edu>
2025-06-11 14:07:58 -0500

    12-June-2025

--- examples/example9.cpp
+++ examples/example9.cpp
@@ -1,4 +1,4 @@
-/* Date: 9th June, 2025 */
+/* Date: 11th June, 2025 */
 /* Author: Sylvia Dogbatsey */
 
 /* To run the program in a MACOS or Windows terminal, use the following command: */
@@ -17,9 +17,31 @@ using namespace std;
 int main() {
 
     int numberDay;
-    cout<<"Enter any number from 1 to 7 corresponding to the da
-
-
+    cout<<"Enter any number from 1 to 7 corresponding to the day of the week. Monday is represented by 1. Tuesday is 2 and so on.\n";
+    cin>>numberDay;
+    switch (numberDay){
+        case 1:
+            cout<<"Monday\n";
+            break;
+        case 2:
+            cout<<"Tuesday\n";
+            break;
+        case 3:
+            cout<<"Wednesday\n";
+            break;
+        case 4:
+            cout<<"Thursday\n";
+            break;
+        case 5:
+            cout<<"Friday\n";
+            break;
+        case 6:
+            cout<<"Saturday\n";
+            break;
+        case 7:
+            cout<<"Sunday\n";
+            break;
+    }
 
     return 0;
 }
